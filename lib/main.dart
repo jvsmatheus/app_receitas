d8a7f301788a11eeda5f76f51f37c9bdf8c6a09c
Matheus Santos <matheus@webfoat.com.br>
2024-10-17 23:13:58 -0300

    initial commit + splash screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:app_receitas/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(), // A splash screen será carregada primeiro
+    );
+  }
+}
