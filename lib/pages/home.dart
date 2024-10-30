fd63b17c8e0ce93b5bcba634f83659fdba6e6bbb
Matheus José Vieira dos Santos <matheusjvs_@hotmail.com>
2024-10-30 18:52:11 -0300

    arrumando home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,31 +15,6 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
 
-    return const Scaffold(
-    appBar: AppBarCustom(),
-    body: Flex(
-      direction: Axis.horizontal,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-          child: Text(
-            "RECEITAS",
-            style: TextStyle(
-              fontSize: 24, // Aumenta o tamanho da fonte para dar destaque
-              fontWeight: FontWeight.bold, // Deixa o texto negrito
-              letterSpacing:
-                  2, // Adiciona espaçamento entre as letras para um efeito estilizado
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ],
-    ),
-    bottomNavigationBar: MenuBarCustom(),
-    );
-
-
     return Scaffold(
       appBar: const AppBarCustom(),
       body: Padding(
@@ -108,4 +83,7 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: const MenuBarCustom(),
     );
   }
+
+
+    
 }
